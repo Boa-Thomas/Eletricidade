553c2634422cc0e18ed709886601777aff0235eb
Thomas Topfstedt <thotop100@gmail.com>
2022-02-15 10:18:10 -0300

    q6 progress

--- Q6.py
+++ Q6.py
@@ -14,9 +14,26 @@ R1 = 0.4
 R2L = 0.2
 X1 = 2.0
 X2L = X1
-escorregamento = 0.02
+s = 0.02 #escorregamento
 print("Corrente no estator")
 
 Vf = vlinha/cmath.sqrt(3)
-Vf = round(Vf,3)
-print("Vf = ", Vf, "V")
+Vfreal = round(Vf.real,3)
+print("Vf = ", Vfreal, "V")
+
+Rp = Vf*Vf/potency
+Rpreal = round(Rp.real,3)
+print("Rp = ", Rpreal, "Ohm")
+
+Xm = Vfreal*Vfreal/cmath.sqrt((Vfreal*current)*(Vfreal*current)-potency*potency)
+Xmreal = round(Xm.real,3)
+print("Xm = ", Xmreal, "Ohm")
+
+Zn = Rp*complex(0,Xm)/complex(Rp,Xm)
+print("Zn = ", Zn, "Ohm")
+
+Z2= R2L/s+complex(0,X2L)
+print("Z2 = ", Z2, "Ohm")
+
+Z1 = R1+complex(0,X1)
+print("Z1 = ", Z1, "Ohm")
